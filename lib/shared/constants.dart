bdec60fd4e0e114f6c5c76089d80870c71ab7894
Mounikausr <mm8737665@gmail.com>
2023-04-03 11:54:08 +0530

    my first commit

---
+++ lib/shared/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  // static String appId = "1:634480644405:web:5549258ea987afaefb7c7c";
+  // static String apiKey = "AIzaSyADZ6Uq-pFQIWMdkGeh7JIozEY7jJM6r_I";
+  // static String messagingSenderId = "634480644405";
+  // static String projectId = "chatappflutter-13318";
+  static String appId = "1:1078460833504:web:d8fb7f6b23178dda737a85";
+static String apiKey = "AIzaSyBcw2RruVPM0e4SBkakRNkURtKtROSklV0";
+static String messagingSenderId = "1078460833504";
+static String projectId = "chatmeflutterapp";
+  final primaryColor = const Color(0xFFee7b64);
+}
