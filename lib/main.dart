d0992aa415bbef0f3e9b76654897e2316db5fd09
Mounikausr <mm8737665@gmail.com>
2023-04-17 17:54:15 +0530

    pulled changes from dev

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,10 @@
-<<<<<<< HEAD
-import 'package:chatme/helper/globalNotification.dart';
-=======
 import 'package:chatme/firebase_options.dart';
->>>>>>> 6c39d3c9793d9f0303bcdf013453085934e51f3a
 import 'package:chatme/helper/helper_function.dart';
 import 'package:chatme/pages/auth/login_page.dart';
 import 'package:chatme/pages/home_page.dart';
 import 'package:chatme/shared/constants.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-<<<<<<< HEAD
-import 'package:flutter/foundation.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-=======
->>>>>>> 6c39d3c9793d9f0303bcdf013453085934e51f3a
 import 'package:flutter/material.dart';
 import 'package:overlay_support/overlay_support.dart';
 // import 'package:flutter_local_notifications/flutter_local_notifications.dart';
@@ -41,19 +32,6 @@ void main() async {
   /*if (kIsWeb) {
     await Firebase.initializeApp(
         options: FirebaseOptions(
-<<<<<<< HEAD
-            apiKey: Constants.apiKey,
-            appId: Constants.appId,
-            messagingSenderId: Constants.messagingSenderId,
-            projectId: Constants.projectId));
-  } else {
-    await Firebase.initializeApp();
-    await GlobalNotificatioSetup().registerNotification();
-
-
-  }
-
-=======
             apiKey: Constants.apiKey, appId: Constants.appId, messagingSenderId: Constants.messagingSenderId, projectId: Constants.projectId, authDomain: Constants.authDomain));
   } else {
     await Firebase.initializeApp();
@@ -64,7 +42,6 @@ void main() async {
       sound: true,
     );
   }*/
->>>>>>> 6c39d3c9793d9f0303bcdf013453085934e51f3a
   //await NotificationService.initializeNotification();
 
   runApp(const MyApp());
