b50b9e4af308ebb614fc02e66f48bd4f4e3a6f20
divyarani123 <info.divya24@gmail.com>
2023-05-16 11:30:02 +0530

    added notification logic in app delegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,25 @@
 import UIKit
-import Flutter
+import UserNotifications
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+class AppDelegate: UIResponder {
+  var window: UIWindow?
+}
+
+extension AppDelegate: UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions:
+                        [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    UNUserNotificationCenter.current().requestAuthorization(options: [
+      .badge, .sound, .alert
+    ]) { granted, _ in
+      guard granted else { return }
+
+      DispatchQueue.main.async {
+        application.registerForRemoteNotifications()
+      }
+    }
+
+    return true
   }
 }
